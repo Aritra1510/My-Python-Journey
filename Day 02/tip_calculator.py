ea1a1d7cfe952c8bcca0772690bc0a2b24c5fd28
Aritra Das Shantanu <aritradas6108@gmail.com>
2025-07-01 01:48:21 +0600

    Project 2

--- Day 02/tip_calculator.py
+++ Day 02/tip_calculator.py
@@ -3,5 +3,5 @@ total_bill = float(input("What was the total bill? $"))
 tip = int(input("How much tip would you like to give? 10, 12, or 15? "))
 people = int(input("How many people to split the bill? "))
 bill_with_tip = total_bill + (tip/100)*total_bill
-individual_bill = bill_with_tip / people
-print(f"Each person should pay: ${individual_bill:.2f}")
+individual_bill = round(bill_with_tip / people, 2)
+print(f"Each person should pay: ${individual_bill}")
